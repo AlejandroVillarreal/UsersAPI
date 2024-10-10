1cabfec542f384024bed11747f05d14b1504cbd6
AlexV <alex_avoc@outlook.com>
2024-10-10 17:59:11 -0600

    api working

--- UsersAPI/UserAPI/Program.cs
+++ UsersAPI/UserAPI/Program.cs
@@ -14,8 +14,9 @@ builder.Services.AddCors(options =>
 	options.AddPolicy(name: MyAllowSpecificOrigins,
 					  policy =>
 					  {
-						  policy.WithOrigins("http://localhost:4200/"
-											  );
+						  policy.WithOrigins("http://localhost:4200"
+											  ).AllowAnyHeader() // Allow all headers
+								.AllowAnyMethod(); // Allow all HTTP methods;
 					  });
 });
 
